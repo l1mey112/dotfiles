79a6a000526e9e8c483551d60bb12a09e0278de9
liam-lin <liam12312390@gmail.com>
2022-05-01 14:38:15 +1000

    dsdsds

--- create.sh
+++ create.sh
@@ -18,8 +18,11 @@ sudo apt autoremove -y
 # remove outdated existing versions of rust
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3  -y
 echo "f"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+read
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | shd
 source ~/.bashrc
+echo 'source $HOME/.cargo/env' >> ~/.bashrc
+source $HOME/.cargo/env
 rustup update stable
 cargo install alacritty
 #? --- install alacritty latest version
