52a63517aa7f7d0d81a98ab3087da9b1b0c83c0b
liam-lin <liam12312390@gmail.com>
2022-05-01 18:44:54 +1000

    dsd

--- create.sh
+++ create.sh
@@ -15,6 +15,7 @@ sudo apt install neofetch hsetroot flameshot -y
 #sudo ninja install -C ~/tmp/i3-gaps/build/
 
 wget -L https://github.com/barnumbirr/i3-gaps-debian/releases/download/v4.20.1-1/i3-gaps_4.20.1-1_amd64_bullseye.deb -O i3-gaps.deb
+read
 sudo gdebi i3-gaps.deb
 
 #? i3 main package deps
