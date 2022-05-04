d60f413877752199aaea0d0332865ad1a965e1f6
liam-lin <liam12312390@gmail.com>
2022-05-05 02:13:59 +1000

    sdsd

--- fonts/font-install.sh
+++ fonts/font-install.sh
@@ -1,7 +1,7 @@
 cd $(dirname "$BASH_SOURCE")
 
-font_location="$HOME/.local/share/fonts/NerdFonts"
-mkdir -p $font_location
-find . -type f -name "*.zip" | xargs unzip -d $font_location
+font_location="/usr/share/fonts/NerdFonts"
+sudo mkdir -p $font_location
+find . -type f -name "*.zip" | xargs sudo unzip -d $font_location
 
 fc-cache -f -v
