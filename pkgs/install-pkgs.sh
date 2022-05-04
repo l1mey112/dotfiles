714130a5fdd4796c98b0bfc9c87c3b731e552a1d
liam-lin <liam12312390@gmail.com>
2022-05-05 01:30:26 +1000

    dsds

--- pkgs/install-pkgs.sh
+++ pkgs/install-pkgs.sh
@@ -5,15 +5,15 @@ sudo apt update
 echo -e "\e[0;36m### installing packages ###\e[0m\n"
 
 echo -e "\e[0;31m### snap apps ###\e[0m"
-cat snap | xargs -d '\n' sudo snap install
+cat snap | xargs -d '\n' sudo snap install --no-wait
 echo -e "\e[0;31m### required apps ###\e[0m"
-cat required | xargs -d '\n' sudo apt install
+cat required | xargs -d '\n' sudo apt install -y
 echo -e "\e[0;31m### required gui apps ###\e[0m"
-cat gui | xargs -d '\n' sudo apt install
+cat gui | xargs -d '\n' sudo apt install -y
 
 if [ "$1" = "laptop" ]; then
     echo -e "\e[0;31m### laptop required ###\e[0m"
-    cat laptop | xargs -d '\n' sudo apt install
+    cat laptop | xargs -d '\n' sudo apt install -y
 fi
 
 echo -e "\e[0;36m### installing external applications ###\e[0m\n"
