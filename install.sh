9ba20e13efff14707d9858a4b157accbbd3591cf
liam-lin <liam12312390@gmail.com>
2022-05-05 01:51:10 +1000

    dsds

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ sudo cp bin/* /usr/local/bin/
 echo -e "\e[0;35m### copying dotfiles ###\e[0m"
 mkdir -p ~/.config/
 cp -r dotconfig/* ~/.config/ 
-echo -e "\e[0;35m### 'moving' home dotfiles ###\e[0m"
-mv home/.xinitrc ~
-mv home/.Xresources ~
-mv home/.xsettingsd ~
-mv home/.bash_aliases ~
+echo -e "\e[0;35m### copying home dotfiles ###\e[0m"
+cp home/.xinitrc ~      # mv home/.xinitrc ~
+cp home/.Xresources ~   # mv home/.Xresources ~
+cp home/.xsettingsd ~   # mv home/.xsettingsd ~
+cp home/.bash_aliases ~ # mv home/.bash_aliases ~
 
 echo -e "\e[0;35m### finalising ###\e[0m"
 sudo apt update && sudo apt upgrade -y
