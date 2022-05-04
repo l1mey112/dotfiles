b49577eb4594c49060cc1319a0983f685dffff30
liam-lin <liam12312390@gmail.com>
2022-05-05 01:33:49 +1000

    sss

--- pkgs/install-pkgs.sh
+++ pkgs/install-pkgs.sh
@@ -5,7 +5,7 @@ sudo apt update
 echo -e "\e[0;36m### installing packages ###\e[0m\n"
 
 echo -e "\e[0;31m### snap apps ###\e[0m"
-cat snap | xargs -d '\n' sudo snap install --no-wait
+cat snap | xargs -n1 -d '\n' sudo snap install --no-wait
 echo -e "\e[0;31m### required apps ###\e[0m"
 cat required | xargs -d '\n' sudo apt install -y
 echo -e "\e[0;31m### required gui apps ###\e[0m"
