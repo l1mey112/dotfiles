91d1f54167ad253fccb1640f6c1543844752c4a7
liam-lin <liam12312390@gmail.com>
2022-05-05 01:25:33 +1000

    dsds

--- install.sh
+++ install.sh
@@ -16,14 +16,16 @@ echo -e "\e[0;35m### copying dotfiles ###\e[0m"
 mkdir -p ~/.config/
 cp dotconfig/* ~/.config/
 echo -e "\e[0;35m### copying home dotfiles ###\e[0m"
-cp home/.xinitrc ~/.config
-cp home/.Xresources ~/.config
-cp home/.xsettingsd ~/.config
-cp home/.bash_aliases ~/.config
+cp home/.xinitrc ~
+cp home/.Xresources ~
+cp home/.xsettingsd ~
+cp home/.bash_aliases ~
 
 echo -e "\e[0;35m### finalising ###\e[0m"
 sudo apt update && sudo apt upgrade -y
 sudo apt-file update -y
+git config --global user.email "liam12312390@gmail.com"
+git config --global user.name "liaml"
 
 echo -e "\e[0;32m### FINISHED ###\e[0m\n"
 echo welcome to the new world.
